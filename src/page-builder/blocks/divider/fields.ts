92218bb44e980765c2ede01023767f0672c327eb
sjmc11 <sjmc11@gmail.com>
2025-12-08 21:51:50 +0000

    Remove src/page-builder to add as submodule

--- src/page-builder/blocks/divider/fields.ts
+++
@@ -1,8 +0,0 @@
-import { createField } from "vue-wswg-editor";
-
-/**
- * Editor fields configuration for HeroSection component
- */
-export default {
-   margin: createField.margin({ group: "appearance" }),
-};
